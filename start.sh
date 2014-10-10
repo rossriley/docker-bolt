4e508cfdfd9f3afb32891952db3168828b9266ca
Ross Riley <riley.ross@gmail.com>
2014-10-10 12:06:05 +0100

    adjustments to make command compatible with bin/sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 chmod -R 0777 ./app;
-IFS=', ' read -a array <<< "$BOLT_EXT"
-for element in "${array[@]}"
+echo "BOLT_EXT is $BOLT_EXT";
+IFS=","
+set -- $BOLT_EXT
+for element in $@
 do
     echo "Installing extestion $element";
     ./vendor/bin/nut extensions:install "$element";
