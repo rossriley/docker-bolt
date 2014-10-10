b8a1d9ec19e8224c1bb9fc6ceaab219403f6f895
Ross Riley <riley.ross@gmail.com>
2014-10-10 13:36:09 +0100

    use eval to run the script

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 chmod -R 0777 ./app;
-echo "BOLT_EXT is $BOLT_EXT";
 IFS=","
 set -- $BOLT_EXT
 for element in $@
 do
     echo "Installing extestion $element";
-    ./vendor/bin/nut extensions:install $element;
+    eval "./vendor/bin/nut extensions:install $element";
 done
