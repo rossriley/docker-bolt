1783e31f37f0c3fd6643e2b126147cdbe49690f9
Ross Riley <riley.ross@gmail.com>
2014-10-31 16:01:39 +0000

    port new start.sh changes

--- start.sh
+++ start.sh
@@ -7,3 +7,17 @@ do
     echo "Installing extestion $element";
     eval "./vendor/bin/nut extensions:install $element";
 done
+
+if [ -n $BOLT_THEME ]
+then
+    echo "Installing theme $BOLT_THEME";
+    eval "./vendor/bin/nut config:set theme demo";
+    eval "ln -sf ../../extensions/vendor/$BOLT_THEME public/theme/demo"
+else
+  echo -e "BOLT_THEME not set\n"
+fi
+
+if [ -n $BOLT_TITLE ]
+then
+    eval "./vendor/bin/nut config:set sitename '$BOLT_TITLE'";
+fi
