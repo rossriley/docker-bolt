2cd9ba0c86acf42d6e778f07ed6e0685bb789d49
Ross Riley <riley.ross@gmail.com>
2016-05-12 14:13:39 +0100

    verify config

--- .bolt.php
+++ .bolt.php
@@ -5,6 +5,7 @@ $configuration->setPath("web", "public");
 $configuration->setPath("files", "public/files");
 $configuration->setPath("themebase", "public/theme");
 $configuration->getVerifier()->disableApacheChecks();
+$configuration->verify();
 $app = new Bolt\Application(array('resources'=>$configuration));
 putenv('COMPOSER_HOME=' . $app['resources']->getPath('cache/composer'));
 $app->initialize();
