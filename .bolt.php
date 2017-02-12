ec1c915210956346a4d676080926d139593501d1
Ross Riley <riley.ross@gmail.com>
2017-02-12 07:37:58 +0000

    tidy up app launch file

--- .bolt.php
+++ .bolt.php
@@ -7,12 +7,6 @@ $configuration->setPath("themebase", "public/theme");
 $configuration->getVerifier()->disableApacheChecks();
 $configuration->verify();
 $app = new Bolt\Application(array('resources'=>$configuration));
-putenv('COMPOSER_HOME=' . $app['resources']->getPath('cache/composer'));
 $app->initialize();
 
-$config = [
-    'application' => $app,
-    'resources' => null,
-];
-
-return $config;
+return $app;
